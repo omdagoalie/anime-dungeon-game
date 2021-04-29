77c13052fb8eb452c763d696b92effefe02d8443
omdagoalie <63820284+omdagoalie@users.noreply.github.com>
2021-04-29 12:57:49 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animeNaruto()
+    Rasen_Shuriken = sprites.createProjectileFromSprite(assets.image`rasen shuriken`, naruTo, 100, 0)
+})
+function animeNaruto () {
     animation.runImageAnimation(
     naruTo,
     assets.animation`naru throw`,
     200,
     false
     )
-    Rasen_Shuriken = sprites.createProjectileFromSprite(assets.image`rasen shuriken`, naruTo, 100, 0)
-})
+}
 let Rasen_Shuriken: Sprite = null
 let naruTo: Sprite = null
 game.showLongText("Welcome to anime escape, In this game you will be teleported to three universes.", DialogLayout.Center)
